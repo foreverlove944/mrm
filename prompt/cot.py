b189a567802f4cd9374a028f409ea6a190a85bcd
foreverlove944 <79123282+foreverlove944@users.noreply.github.com>
2024-08-23 20:59:55 +0800

    Update cot.py

--- prompt/cot.py
+++ prompt/cot.py
@@ -1,2 +1,31 @@
 prompt = """
+Q: When was Neville A. Stanton's employer founded?
+A: The employer of Neville A. Stanton is University of Southampton. The University of Southampton was founded in 1862. So the answer is: 1862.
+
+Q: What is the headquarters for the organization who sets the standards for ISO 21500?
+A: The standards for ISO 21500 were set by International Organization for Standardization. The International Organization for Standardization has headquarters in Geneva. So the answer is: Geneva.
+
+Q: What region of the state where Guy Shepherdson was born, contains SMA Negeri 68?
+A: Guy Shepherdson was born in Jakarta. SMA Negeri 68 Jakarta is located in Central Jakarta. So the answer is: Central Jakarta.
+
+Q: When was the first railway line constructed between Kotri and the city where Marie Adelaide Leprosy Centre is located?
+A: Marie Adelaide Leprosy Centre is located in Karachi. The first railway line between Kotri and Karachi was constructed in April 1858. So the answer is: April 1858.
+
+Q: What county is Hebron located in, in the same province the Heritage Places Protection Act applies to?
+A: Heritage Places Protection Act applies to the jurisdiction of Prince Edward Island. Hebron, Prince Edward Island is located in the Prince County. So the answer is: Prince County.
+
+Q: When did the first large winter carnival take place in the city where CIMI-FM is licensed to broadcast?
+A: CIMI-FM is licensed to broadcast in Quebec City. The first large winter carnival in Quebec City took place in 1894. So the answer is: 1894.
+
+Q: When did the first large winter carnival happen in Olivier Robitaille's place of birth?
+A: Olivier Robitaille was born in Quebec City. The first large winter carnival in Quebec City happened in the 1894. So the answer is: 1894.
+
+Q: When did Britain withdraw from the country containing Hoora?
+A: Hoora is in the country of Bahrain. Britain withdrew from Bahrain in 1971. So the answer is: 1971.
+
+Q: What did the publisher of Banjo-Tooie rely primarily on for its support?
+A: The publisher of Banjo-Tooie is Nintendo. Nintendo relied primarily for its support on first-party games. So the answer is: first-party games.
+
+Q: What shares a border with Rivière-Verte in the province WRSU-FM broadcasts in?
+A: WRSU-FM was licensed to broadcast to New Brunswick. Rivière-Verte, New Brunswick shares border with Edmundston. So the answer is: Edmundston.
 """
